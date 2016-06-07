3df4479021049aa035a75bd5a3b3c699c791243b
kevin <kevinochieng548@gmail.com>
2016-06-07 10:02:36 +0300

    add method to forget users

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,14 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-     #Returns true if the 
+     #Returns true if the given token matches the digest
 	def authenticated?(remember_token)
+		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	#forget a user
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
