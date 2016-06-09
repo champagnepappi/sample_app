f5420ada62416bfa95f7de9c1d3e99c775fdc799
kevin <kevinochieng548@gmail.com>
2016-06-09 12:26:30 +0300

    add microposts to sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,9 @@ User.create!(name: "Kevin Santos", email: "kevinochieng548@gmail.com", password:
 	password = "password"
 	User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+	content = Faker::Lorem.sentence(5)
+	users.each { |user| user.microposts.create!(content: content) }
+end
