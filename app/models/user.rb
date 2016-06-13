8168ca89256b3772cd74653fb02ffa4f6bc9bc5b
kevin <kevinochieng548@gmail.com>
2016-06-13 10:17:57 +0300

    add account activation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase}
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_create :create_activation_digest
 	validates(:name, presence: true, length: {maximum: 50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates(:email, presence: true, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false})
@@ -41,4 +42,17 @@ class User < ActiveRecord::Base
 	def feed
 		Micropost.where("user_id = ?", id)
 	end
+
+	private
+
+	 #converts email to all lower-case
+	 def downcase_email
+	 	self.email = email.downcase_email
+	 end
+
+	 #Creates and assigns activation token and digest
+	 def create_activation_digest
+	 	self.activation_token = User.new_token
+	 	self.activation_digest = User.digest(activation_token)
+	 end
 end
