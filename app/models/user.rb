9a4207f4c30368bda1eae7ee28b2ebe8b85aaeca
kevin <kevinochieng548@gmail.com>
2016-06-13 15:07:51 +0300

    add user activation methods to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ActiveRecord::Base
 		BCrypt::Password.new(digest).is_password?(token)
 	end
 
+
 	#forget a user
 	def forget
 		update_attribute(:remember_digest, nil)
@@ -42,6 +43,17 @@ class User < ActiveRecord::Base
 
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	# Activates an account
+	def activate
+		update_attribute(:activated,  true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	# Sends activation email
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
 	end
 
 	private
