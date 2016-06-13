101e7234fa7b690d60dcf44e1573603a8d14a380
kevin <kevinochieng548@gmail.com>
2016-06-13 10:23:21 +0300

    activate seed users by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Kevin Santos", email: "kevinochieng548@gmail.com", password: "password", password_confirmation: "password", admin: true)
+User.create!(name: "Kevin Santos", 
+	         email: "kevinochieng548@gmail.com",
+	          password: "password", 
+	          password_confirmation: "password", 
+	          admin: true,
+	          activated: true,
+	          activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n+1}@gmail.com"
 	password = "password"
-	User.create!(name: name, email: email, password: password, password_confirmation: password)
+	User.create!(name: name, 
+		        email: email,
+		        password: password, 
+		        password_confirmation: password,
+		        activated: true,
+		        activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
