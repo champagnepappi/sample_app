4242d35b1d350b05436356ca27673a679c874f6d
kevin <kevinochieng548@gmail.com>
2016-06-14 11:16:04 +0300

    mail password reset links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  #get 'password_resests/new'
+  # get 'password_resets/new'
 
-  #get 'password_resests/edit'
+  # get 'password_resets/edit'
 
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
