2afb5b8adaac83a4d53d60ddaf69697fd856c639
kevin <kevinochieng548@gmail.com>
2016-06-16 10:18:53 +0300

    add forms for following and unfollowing user

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,10 @@ Rails.application.routes.draw do
     end
   end
   resources :account_activations, only: [:edit]
-  resources :microposts,          only: [:create, :destroy]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+ 
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
