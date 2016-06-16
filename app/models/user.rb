1ea74aa281f4c7c001dc3856ab5a64d38589ed0a
kevin <kevinochieng548@gmail.com>
2016-06-16 08:51:54 +0300

    add following and followers actions to Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,12 +55,12 @@ class User < ActiveRecord::Base
 
 	# Follows a user
 	def follow(other_user)
-       active_relationships.create(followed_id: other_user_id)
+       active_relationships.create(followed_id: other_user.id)
 	end
 
 	# Unfollows a user
 	def unfollow(other_user)
-		active_relationships.find_by(followed_id: other_user_id).destroy
+		active_relationships.find_by(followed_id: other_user.id).destroy
 	end
 
 	# Returns true if the current user is following the other user.
